cc0ae83393f1cb3b5773332aa56dd22045362e4d
Gaurav-py-Ghosh <Gauravghosh24005@gmail.com>
2025-01-13 19:56:52 +0530

    bugs

--- src/components/MeetTheTeam/Core.jsx
+++ src/components/MeetTheTeam/Core.jsx
@@ -2,9 +2,10 @@ import TeamCards from "./TeamCards";
 
 const Core = () =>{
     return(
-        <div className="flex flex-col md:flex-row md:flex-wrap justify-center md:justify-around items-center md:space-y-0">
-            <TeamCards img={"/assets/AdityaBansal.png"} name={"Aditya bansal"} about={"Silent coder, life debugger—perfecting systems and situations, even in the sleep."} github={"http://github.com/SimplyAditya"} linkedin={"http://linkedin.com/in/aditya-bansal-developer"}/>
+        <div className="grid md:grid-cols-3 grid-cols-2 gap-x-10 gap-y-1">
+            
             <TeamCards img={"/assets/AdityaRastogi.png"} name={"Aditya Rastogi"} about={"Anxious yet adorable—panics hard, but wins harder with a smile that lights up the room."} github={"http://github.com/AdityaR-Dev98"} linkedin={"http://linkedin.com/in/aditya-rastogi-960116226"}/>
+            <TeamCards img={"/assets/AdityaBansal.png"} name={"Aditya bansal"} about={"Silent coder, life debugger—perfecting systems and situations, even in the sleep."} github={"http://github.com/SimplyAditya"} linkedin={"http://linkedin.com/in/aditya-bansal-developer"}/>
             <TeamCards img={"/assets/Gaurangi.png"} name={"Gaurangi"} about={"Fierce on the outside, sweet on the inside—precision and perfection are always my vibe."}/>
             <TeamCards img={"/assets/Shrey.png"} name={"Shrey Jaiswal"} about={"Turning problems into punchlines—too cool to freak out, but ready to freak out the odds."} github={"https://github.com/jaisw07"} linkedin={"https://www.linkedin.com/in/shrey-jaiswal-1a7724272/"}/>
             <TeamCards img={"/assets/Priyal.png"} name={"Shrey Jaiswal"} about={"Powered by caffeine and pure passion— my calm exterior hides a workaholic on a mission to make everything better."} github={"https://github.com/priyalkhullar"} linkedin={"https://www.linkedin.com/in/priyal-khullar"}/>
--- src/components/MeetTheTeam/TeamNav.jsx
+++ src/components/MeetTheTeam/TeamNav.jsx
@@ -1,7 +1,7 @@
-import { useState,lazy,Suspense } from "react";
+import { useState, lazy, Suspense } from "react";
 import { motion } from "motion/react";
 
-const Core = lazy (()=>import("./Core") )
+const Core = lazy(() => import("./Core"));
 const Operations = lazy(() => import("./Operations"));
 const On_Marketing = lazy(() => import("./On_Marketing"));
 const Off_Marketing = lazy(() => import("./Off_Marketing"));
@@ -14,32 +14,33 @@ const Entry = lazy(() => import("./Entry"));
 const Sponsorship = lazy(() => import("./Sponsorship"));
 
 const items = [
-  { id: "core", label: "Core",component: Core },
-  { id: "operations", label: "Operations",component: Operations },
-  { id: "On_marketing", label: "Online Marketing",component: On_Marketing },
-  { id: "Off_marketing", label: "Offline Marketing",component: Off_Marketing},
-  { id: "content", label: "Content",component: Content },
-  { id: "design", label: "Design",component: Design },
-  { id: "coverage", label: "Coverage",component:Coverage},
-  { id: "technical", label: "Technical",component: Technical },
-  { id: "judges", label: "Judges & PR",component: Judges },
-  { id: "Entry", label: "Entry Management",component: Entry },
-  { id: "sponsorship", label: "Sponsorship",component: Sponsorship },
+  { id: "core", label: "Core", component: Core },
+  { id: "operations", label: "Operations", component: Operations },
+  { id: "On_marketing", label: "Online Marketing", component: On_Marketing },
+  { id: "Off_marketing", label: "Offline Marketing", component: Off_Marketing },
+  { id: "content", label: "Content", component: Content },
+  { id: "design", label: "Design", component: Design },
+  { id: "coverage", label: "Coverage", component: Coverage },
+  { id: "technical", label: "Technical", component: Technical },
+  { id: "judges", label: "Judges & PR", component: Judges },
+  { id: "Entry", label: "Entry Management", component: Entry },
+  { id: "sponsorship", label: "Sponsorship", component: Sponsorship },
 ];
 const TeamNav = () => {
   const [Clicked, setClicked] = useState(items[0].id);
 
-  const CurrentComponend = items.find(item=>item.id === Clicked)?.component;
+  const CurrentComponend = items.find((item) => item.id === Clicked)?.component;
   return (
     <div className="flex justify-center items-center px-4 md:mb-16">
-      <div className="flex flex-wrap justify-center items-center w-full md:w-[80%]">
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-wrap justify-center items-center w-[80%]">
         {items.map((item) => (
           <button
             key={item.id}
             onClick={() => setClicked(item.id)}
             className={`${
               Clicked === item.id ? "" : "hover:text-white/60"
-            } relative rounded-full px-5 md:px-8 md:py-4 py-3 text-xl font-medium text-white`}
+            } relative rounded-full px-5 md:px-8 md:py-4 py-3 md:text-2xl lg:text-4xl text-xl font-medium text-white`}
           >
             {Clicked === item.id && (
               <motion.span
@@ -48,22 +49,29 @@ const TeamNav = () => {
                 transition={{ type: "spring", bounce: 0.2, duration: 0.8 }}
               />
             )}
-            <span className="text-base md:text-xl">{item.label}</span>
+            <span className="text-base md:text-xl whitespace-nowrap">
+              {item.label}
+            </span>
           </button>
         ))}
-         <Suspense fallback={<div className="relative w-full m-80 p-10 animate-pulse"></div>}>
-            <motion.div
-              key={Clicked}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{duration:0.3,  ease: "easeInOut"}}
-            >
-              {CurrentComponend && <CurrentComponend />}
-            </motion.div>
-          </Suspense>
+        </div>
+        <Suspense
+          fallback={
+            <div className="relative w-full m-80 p-10 animate-pulse"></div>
+          }
+        >
+          <motion.div
+            key={Clicked}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2, ease: "easeInOut" }}
+          >
+            {CurrentComponend && <CurrentComponend />}
+          </motion.div>
+        </Suspense>
       </div>
-      </div>
+    </div>
   );
 };
 export default TeamNav;
