9d3450bd572fa7688d1270ff8ade5fba9bf6445a
Gaurav-py-Ghosh <Gauravghosh24005@gmail.com>
2025-01-15 14:56:31 +0530

    names fixed

--- src/components/MeetTheTeam/Operations.jsx
+++ src/components/MeetTheTeam/Operations.jsx
@@ -3,7 +3,7 @@ import { memo } from "react";
 const Operations = () => {
     return(
         <div className="flex flex-col md:flex-row md:gap-20 justify-center md:justify-evenly items-center">
-            <TeamCards name={"Tamnay"} img={"/assets/Tanmay.png"} about={"After 10, I’m all in: coordinating chaos, perfecting operations, and delivering roasts sharper than my timelines."}/>
+            <TeamCards name={"Tanmay"} img={"/assets/Tanmay.png"} about={"After 10, I’m all in: coordinating chaos, perfecting operations, and delivering roasts sharper than my timelines."}/>
             <TeamCards name={"Ayush"} img={"/assets/Ayush.png"} about={"Building the backbone, I make sure that everything gets the momentum it needs."} github={"https://github.com/Ayush1075"} linkedin={"https://www.linkedin.com/in/ayush-gupta-07612928b/"}/>
         </div>
     )
