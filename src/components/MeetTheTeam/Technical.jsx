adb612433d09506d434c2b2096e68733d08e5da7
Gaurav-py-Ghosh <Gauravghosh24005@gmail.com>
2025-01-15 17:54:22 +0530

    sejal img fix

--- src/components/MeetTheTeam/Technical.jsx
+++ src/components/MeetTheTeam/Technical.jsx
@@ -4,7 +4,7 @@ const Technical = () =>{
     return(
         <div className="flex flex-col md:flex-row md:gap-20 justify-center md:justify-evenly items-center">
             <TeamCards name={"Gaurav Ghosh"} img={"/assets/gaurav.webp"} about={"Tech wizard in a hurry—confirm it fast, and I’ll deliver even faster."} github={"https://github.com/Gaurav-py-Ghosh"} linkedin={"https://www.linkedin.com/in/gaurav-ghosh-9531132b3/"} />
-            <TeamCards name={"Sejal Gupta"} img={"/assets/sejal.webp"} about={"If it’s assigned to me, it’s as good as done—dedication on autopilot."}  github={"https://github.com/Sejal207"} linkedin={"https://www.linkedin.com/in/sejal-gupta-943832291/"}/>
+            <TeamCards name={"Sejal Gupta"} img={"/assets/Sejal.webp"} about={"If it’s assigned to me, it’s as good as done—dedication on autopilot."}  github={"https://github.com/Sejal207"} linkedin={"https://www.linkedin.com/in/sejal-gupta-943832291/"}/>
         </div>
     )
 }
